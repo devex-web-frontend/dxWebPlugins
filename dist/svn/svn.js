f0165bb45159d87ac2efd14bb422a9ea9c5c7e8b
mankdev <tema@mankdev.name>
2015-04-25 17:51:40 +0300

    add svn release helper

--- dist/svn/svn.js
+++ dist/svn/svn.js
@@ -11,23 +11,58 @@ var SVNHelper = (function () {
 	function SVNHelper(cwd) {
 		_classCallCheck(this, SVNHelper);
 
-		console.log('svn-constructor', cwd);
+		this.SVNSpawn = new SVN(cwd);
 	}
 
 	_createClass(SVNHelper, [{
 		key: 'add',
-		value: function add(pattern) {
-			console.log('add');
+		value: function add(files) {
+			var _this = this;
+
+			return new Promise(function (resolve, reject) {
+				return _this.SVNSpawn.add(files, function (err, data) {
+					err ? reject(err) : resolve(data);
+				});
+			});
 		}
 	}, {
 		key: 'commit',
-		value: function commit(tag) {
-			console.log('commit');
+		value: function commit(comment) {
+			var _this2 = this;
+
+			return new Promise(function (resolve, reject) {
+				return _this2.SVNSpawn.commit(comment, function (err, data) {
+					err ? reject(err) : resolve(data);
+				});
+			});
+		}
+	}, {
+		key: 'getInfo',
+		value: function getInfo() {
+			var _this3 = this;
+
+			return new Promise(function (resolve, reject) {
+				return _this3.SVNSpawn.getInfo(function (err, data) {
+					err ? reject(err) : resolve(data);
+				});
+			});
 		}
 	}, {
 		key: 'createTag',
 		value: function createTag(tagName) {
-			console.log('tagname');
+			var _this4 = this;
+
+			return new Promise(function (resolve, reject) {
+				return _this4.getInfo().then(function (info) {
+					var trunkFolder = info.url;
+					var tagsFolder = trunkFolder.replace('trunk', 'tags');
+					var destination = tagsFolder + '/' + tagName;
+
+					return _this4.SVNSpawn.cmd(['copy', trunkFolder, destination, '-m="tag ' + tagName + '"'], function (err, data) {
+						err ? reject(err) : resolve(data);
+					});
+				});
+			});
 		}
 	}]);
 
