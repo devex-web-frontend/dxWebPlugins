6986cc1c72daa5350258781d1c9a88f1b70245ec
cakeinpanic <cakeinpanic@gmail.com>
2015-04-25 17:00:20 +0300

    Page editing

--- src/confluence/login.js
+++ src/confluence/login.js
@@ -8,6 +8,7 @@ function createRequest(path ,method){
         port: 443,
         contentType:"application/json; charset=utf-8",
         'path': path,
+        method: method || "GET",
         headers: {
             'Authorization': 'Basic ' + auth
         },
@@ -16,9 +17,9 @@ function createRequest(path ,method){
         agent: false
     };
 }
-
+var version;
 function getPageContent(pageId, resolve, reject) {
-    var path = '/rest/api/content/' + pageId + '?expand=body.view';
+    var path = '/rest/api/content/' + pageId + '?expand=body.view,version';
         https.get(createRequest(path), function(res) {
             var respond = '';
             res.on('data', function(chunk) {
@@ -26,12 +27,49 @@ function getPageContent(pageId, resolve, reject) {
             });
             res.on('end', function() {
                 var result = JSON.parse(respond);
-                console.log(result.body.view)
+                var body = result.body.view;
+                version = result.version.number;
+                console.log(result)
             });
-
-
-
     });
+
 }
 
-getPageContent(108139543);
+
+function setPageContent(pageId) {
+    var path = '/rest/api/content/' + pageId,
+        req = createRequest(path, 'PUT'),
+        data = {
+            "id": pageId,
+            "type": "page",
+            "version": {number:3},
+            "title": 'test',
+            "body":{
+                "view": {
+                    "value": "<p>This is a new page</p>"
+                }
+            }
+        };
+        data = JSON.stringify(data);
+        console.log(data)
+    req.headers['Content-Type'] ='application/json';
+        req.headers['Content-Length'] = data.length;
+    var R = https.request(req, function(res) {
+        var respond = '';
+        res.on('data', function(chunk) {
+            respond += chunk;
+        });
+        res.on('end', function() {
+            var result = JSON.parse(respond);
+            console.log(result);
+        });
+
+    });
+    R.write(data);
+    R.end();
+}
+
+
+
+//getPageContent(108139543);
+setPageContent(108139543);
