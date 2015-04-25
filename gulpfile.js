46b39af23ea7fe50fbb47dce47b3ab0da26d02ee
cakeinpanic <cakeinpanic@gmail.com>
2015-04-25 15:15:52 +0300

    Add gulp crash sound

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
 	babel = require('gulp-babel'),
 	watch = require('gulp-watch'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+    gCrashSound = require('gulp-crash-sound');
+
+
+gCrashSound.config({
+    file: './gulpCrashSound.mp3'
+});
 
 gulp.task('default', function() {
 	gulp.src('./src/**/*.js')
