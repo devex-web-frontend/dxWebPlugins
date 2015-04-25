3d366c779636b1f6bf9714ddb4e3a72276fbaf8f
cakeinpanic <cakeinpanic@gmail.com>
2015-04-25 16:35:11 +0300

    Get info

--- dist/confluence/login.js
+++ dist/confluence/login.js
@@ -1,15 +1,17 @@
+'use strict';
+
 var https = require('https');
 var conf = require('../../conf.json');
 
-function createRequest(path ,method){
+function createRequest(path, method) {
     var auth = new Buffer(conf.user + ':' + conf.pass).toString('base64');
     return {
         host: 'confluence.in.devexperts.com',
         port: 443,
-        contentType:"application/json; charset=utf-8",
-        'path': path,
+        contentType: 'application/json; charset=utf-8',
+        path: path,
         headers: {
-            'Authorization': 'Basic ' + auth
+            Authorization: 'Basic ' + auth
         },
         rejectUnauthorized: false,
         requestCert: true,
@@ -19,18 +21,15 @@ function createRequest(path ,method){
 
 function getPageContent(pageId, resolve, reject) {
     var path = '/rest/api/content/' + pageId + '?expand=body.view';
-        https.get(createRequest(path), function(res) {
-            var respond = '';
-            res.on('data', function(chunk) {
-                respond += chunk;
-            });
-            res.on('end', function() {
-                var result = JSON.parse(respond);
-                console.log(result.body.view)
-            });
-
-
-
+    https.get(createRequest(path), function (res) {
+        var respond = '';
+        res.on('data', function (chunk) {
+            respond += chunk;
+        });
+        res.on('end', function () {
+            var result = JSON.parse(respond);
+            console.log(result.body.view);
+        });
     });
 }
 
